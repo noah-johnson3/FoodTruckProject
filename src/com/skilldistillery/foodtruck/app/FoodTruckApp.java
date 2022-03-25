9c9374e68ae3d640923ed617b37447e0ef9e833e
noah-johnson3 <noah.johnson3@yahoo.com>
2022-03-25 18:37:41 -0400

    initial commit

---
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,17 @@
+package com.skilldistillery.foodtruck.app;
+
+public class FoodTruckApp {
+
+	public static void main(String[] args) {
+		
+		// array of 5 food trucks
+		// scanner for name, food type, etc
+		//display trucks ID
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.foodtruck.entities;
+
+public class FoodTruck {
+	
+	private static int nextTruckId;
+	private String truckName;
+	private String foodType;
+	private double rating;
+	
+
+}
